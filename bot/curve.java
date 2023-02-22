b270cd2f7b104821eb43ea3829f53a15a9a00faa
Lukas Fohl <lulu@DESKTOP-C3VPI5C.localdomain>
2023-02-22 11:41:54 +0100

    curve idea

---
+++ bot/curve.java
@@ -0,0 +1,13 @@
+package bot;
+
+import framework.*;
+
+public class curve{
+    //2d array + curve with f(x)=2^(x)
+    //origin 0;1
+    //from current point
+    //->next    x = x (current) + 1
+    //          y = 2y(current) - 1 
+    //->last    x = x (current) - 1
+    //          y = (y(current)%2==0)?((y(current)-1)/2):((y(current))/2)
+}
